e3be010e1404f5b56b92b7575adb571c29147f57
klava8320-wq <klava8320@gmail.com>
2026-01-09 17:27:28 +0300

    Add files via upload

---
+++ LR_3/task_1.py
@@ -0,0 +1,12 @@
+# TODO Напишите функцию для поиска индекса товара
+items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
+for find_item in ['банан', 'груша', 'персик']:
+    if find_item in items_list:
+        index_item=items_list.index(find_item)
+        print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
+        continue
+    else:
+        print(f"Товар '{find_item}' не найден в списке.")
+        continue
+   # if index_item is not None:
+    ###  print(f"Товар '{find_item}' не найден в списке.")
